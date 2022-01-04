1399a580f56bcde51d6b58b55c86a783a6e1f5e1
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-01-04 12:19:30 +0300

    pymongo crud example added

---
+++ examples/example_Crud_app/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from extviews.connections import PymongoConnection
+from extviews.crudset import PymongoCrudSet
+from extviews import CrudViewSet
+
+app = FastAPI()
+
+#------------------------------------------------------------------------------
+# Models
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+#------------------------------------------------------------------------------
+# CRUD SET
+
+class UserCrudSet(PymongoCrudSet):
+    connection = PymongoConnection
+    collection = "users"
+    model = User
+
+#------------------------------------------------------------------------------
+# VIEW SET
+
+class UserCrudViewSet(CrudViewSet):
+    base_path = '/users'
+    class_tag = 'user'
+    crud = UserCrudSet
+    model = User
+
+
+app.include_router(UserCrudViewSet())
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", port=8001 ,reload=True, workers=4, )
+
