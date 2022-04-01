9a40aa278dc2cf4cc3c359fe7be671334bdb7122
bilalAlpaslan <m.bilal.alpaslan@gmail.com>
2022-04-01 16:15:42 +0300

    version 0.1.5

--- extviews/__init__.py
+++ extviews/__init__.py
@@ -1,6 +1,6 @@
 """ extviews is a fastapi library for creating RESTful APIs with a single class. """
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .viewset import ViewSet, CrudViewSet
 from .crudset import BaseCrudSet, ModelCrudSet, PymongoCrudSet, MotorCrudSet
